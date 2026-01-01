561613fdc7e7739142bbca4a6afea40fe809ecce
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-01-01 13:08:32 +0530

    Create 3) Plus One.cpp

---
+++ 22) Maths/3) Plus One.cpp
@@ -0,0 +1,22 @@
+/*
+T.C = O(n);
+S.C = O(1);
+*/
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+        int n = digits.size();
+        int carry = 1;
+
+        for(int i=n-1;i>=0;i--){
+           if(digits[i] < 9){
+              digits[i]++;
+              return digits;
+           }
+           digits[i] = 0;
+        }
+        
+        digits.insert(digits.begin(),1);
+        return digits;
+    }
+};
