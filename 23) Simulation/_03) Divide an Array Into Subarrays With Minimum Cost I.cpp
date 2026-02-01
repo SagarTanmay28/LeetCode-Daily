8306f230e878888763feb825eda1b9e2d3f144df
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-02-01 20:07:04 +0530

    Create _03) Divide an Array Into Subarrays With Minimum Cost I.cpp

---
+++ 23) Simulation/_03) Divide an Array Into Subarrays With Minimum Cost I.cpp
@@ -0,0 +1,26 @@
+/*
+T.C = O(n);
+S.C = O(1);
+*/
+class Solution {
+public:
+    int minimumCost(vector<int>& nums) {
+        int n = nums.size();
+
+        int first = INT_MAX;
+        int second = INT_MAX;
+
+        for(int i=1;i<n;i++){
+            if(nums[i] < first){
+                second = first;
+                first = nums[i];
+            }
+            else if(nums[i] < second){
+                second = nums[i];
+            }
+        }
+
+        return nums[0] + first + second;
+
+    }
+};
