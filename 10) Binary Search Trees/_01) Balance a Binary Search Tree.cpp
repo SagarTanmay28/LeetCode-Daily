a10706ec924b0d5b0674a3b9477dbba8a0d3555f
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-02-10 00:49:50 +0530

    Create _01) Balance a Binary Search Tree.cpp

---
+++ 10) Binary Search Trees/_01) Balance a Binary Search Tree.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    void inorder(TreeNode* root, vector<int>& vec){
+        if(root == NULL) return;
+
+        inorder(root->left,vec);
+        vec.push_back(root->val);
+        inorder(root->right,vec);
+    }
+
+    TreeNode* construct(int l, int r, vector<int>& vec){
+        if(l > r) return NULL;
+
+        int mid = l + (r-l)/2;
+
+        TreeNode* root = new TreeNode(vec[mid]);
+        root->left = construct(l,mid-1,vec);
+        root->right = construct(mid+1,r,vec);
+
+        return root;
+    }
+    TreeNode* balanceBST(TreeNode* root) {
+        vector<int> vec;
+        inorder(root,vec);
+
+        int l = 0;
+        int r = vec.size() - 1;
+
+        return construct(l,r,vec);
+    }
+};
