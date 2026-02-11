3a6bdcc052bf7addd8e2bfd5efbc4c4543877cfd
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-02-11 23:36:47 +0530

    Update _01) Balance a Binary Search Tree.cpp

--- 10) Binary Search Trees/_01) Balance a Binary Search Tree.cpp
+++ 10) Binary Search Trees/_01) Balance a Binary Search Tree.cpp
@@ -3,7 +3,7 @@ public:
     void inorder(TreeNode* root, vector<int>& vec){
         if(root == NULL) return;
 
-        inorder(root->left,vec);
+        inorder(root->left,vec); 
         vec.push_back(root->val);
         inorder(root->right,vec);
     }
